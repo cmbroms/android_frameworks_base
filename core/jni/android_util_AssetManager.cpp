327cfadcb318a15e413567b4579104eab076df37
nycbjr <nycbjr@gmail.com>
2013-10-31 21:25:08 -0400

    more inverted ui

--- core/jni/android_util_AssetManager.cpp
+++ core/jni/android_util_AssetManager.cpp
@@ -517,7 +517,8 @@ static void android_content_AssetManager_setConfiguration(JNIEnv* env, jobject c
                                                           jint screenWidth, jint screenHeight,
                                                           jint smallestScreenWidthDp,
                                                           jint screenWidthDp, jint screenHeightDp,
-                                                          jint screenLayout, jint uiMode,
+                                                          jint screenLayout,
+                                                          jint uiInvertedMode, jint uiMode,
                                                           jint sdkVersion)
 {
     AssetManager* am = assetManagerForJavaObject(env, clazz);
@@ -544,6 +545,7 @@ static void android_content_AssetManager_setConfiguration(JNIEnv* env, jobject c
     config.screenWidthDp = (uint16_t)screenWidthDp;
     config.screenHeightDp = (uint16_t)screenHeightDp;
     config.screenLayout = (uint8_t)screenLayout;
+    config.uiInvertedMode = (uint8_t)uiInvertedMode;
     config.uiMode = (uint8_t)uiMode;
     config.sdkVersion = (uint16_t)sdkVersion;
     config.minorVersion = 0;
